851c7cf1951624dc8538695dd3b23c95875d5809
samelnmu <samelnmu@gmail.com>
2024-12-20 19:50:21 +0300

    objectsCommit

---
+++ javaScript/objects.js
@@ -0,0 +1,20 @@
+car = {type : "Benz", model: "Glc", year: 2024};
+
+console.log(car.type);
+
+
+const person = new Object()
+person.firstName = 'Samuel';
+person.middleNmae = 'Ngige';
+person.dreamCar = 'Benz GLC AMG 43';
+person.favFood = 'ugali and pork';
+
+console.log(person.firstName +  ' is a succer of ' + person.dreamCar);
+
+const me = {
+    firstName : 'Samuel',
+    secondName : 'Ngige',
+    lastName : 'Mungai',
+}
+delete me.lastName;
+console.log(me);
