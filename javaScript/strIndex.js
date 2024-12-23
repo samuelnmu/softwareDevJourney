b18aef1aef0f59af8d5eeb0235e0be68d52deaee
samelnmu <samelnmu@gmail.com>
2024-12-23 13:29:33 +0300

    stringCommit

---
+++ javaScript/strIndex.js
@@ -0,0 +1,13 @@
+let text = "Please lock in sir"
+let ind = text.indexOf("lock");
+console.log(ind);
+
+
+let password = "12345"
+let crack = password.includes("1");
+console.log(crack);
+
+let x = 10;
+let y = 20;
+let z = "The prize is " + x + y;
+console.log(z);
